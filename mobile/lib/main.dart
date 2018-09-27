5fb6d6027a6e0b5f110912b3e2e1d041eb3c6a62
Zufar Muhamadeev <zufarm@gmail.com>
2018-09-27 13:31:05 +0300

    update route on swipe refresh

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:mobile/redux/action/route_actions.dart';
 import 'package:mobile/redux/reducers/route_reducers.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'package:redux/redux.dart';
@@ -22,7 +25,21 @@ void main() {
 class FlutterReduxApp extends StatelessWidget {
   final Store<m.Route> store;
 
-  const FlutterReduxApp({Key key, this.store}) : super(key: key);
+  GlobalKey<RefreshIndicatorState> _refreshIndicatorKey = new GlobalKey<RefreshIndicatorState>();
+  FlutterReduxApp({Key key, this.store}) : super(key: key);
+
+  Future<void> _updateRoute() async {
+    var route = new m.Route();
+
+    for (var i = -100; i < 50; ++i) {
+      var task1 = new m.Task();
+      var point1 = new m.Point();
+      point1.name = "point$i name";
+      task1.point = point1;
+      route.tasks.add(task1);
+    }
+    this.store.dispatch(UpdateRouteAction(route));
+  }
 
   // This widget is the root of your application.
   @override
@@ -40,15 +57,19 @@ class FlutterReduxApp extends StatelessWidget {
           body: new StoreConnector<m.Route, m.Route>(
               converter: (store) => store.state,
               builder: (context, route) {
-                return new ListView.separated(
-                  itemCount: route.tasks.length,
-                  itemBuilder: (context, position) => new TaskItem(task: route.tasks[position]),
-                  separatorBuilder: ((context, position) =>
+                return RefreshIndicator(
+                  key: _refreshIndicatorKey,
+                  onRefresh: _updateRoute,
+                  child: ListView.separated(
+                    itemCount: route.tasks.length,
+                    itemBuilder: (context, position) => new TaskItem(task: route.tasks[position]),
+                    separatorBuilder: ((context, position) =>
                     new Padding(
-                      padding: EdgeInsets.only(left: 20.0, right: 20.0),
-                      child:new Container(height: 1.5, color: Colors.grey)
+                        padding: EdgeInsets.only(left: 20.0, right: 20.0),
+                        child:new Container(height: 1.5, color: Colors.grey)
                     )
-                  ),
+                    ),
+                  )
                 );
               }
           )
